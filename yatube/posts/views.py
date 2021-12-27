3ef85ac8758f85604c5fb66268fe50c9476e9d16
vovanbart <vovanbart@icloud.com>
2021-12-27 23:30:51 +0300

    ready

---
+++ yatube/posts/views.py
@@ -0,0 +1,166 @@
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.decorators.cache import cache_page
+
+from .forms import PostForm, CommentForm
+from .models import Group, Post, User, Follow
+
+
+@cache_page(20)
+def index(request):
+    search = request.GET.get('search', '')
+    if search:
+        post_list = Post.objects.filter(text__icontains=search)
+    else:
+        post_list = Post.objects.all()
+    paginator = Paginator(post_list, settings.PAGE_POST)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    group_list = group.posts.all()
+    paginator = Paginator(group_list, settings.PAGE_POST)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    count = group.posts.filter(group=group).count()
+    context = {
+        'group': group,
+        'count': count,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/group_list.html', context)
+
+
+def groups(request):
+    group_list = Group.objects.all()
+    context = {
+        'group_list': group_list
+    }
+    return render(request, 'posts/groups.html', context)
+
+
+@login_required
+def post_create(request):
+    form = PostForm(request.POST or None,
+                    files=request.FILES or None
+                    )
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect('posts:profile', username=request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'posts/create_post.html', context)
+
+
+@login_required
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, pk=post_id, author=request.user)
+    form = PostForm(request.POST or None,
+                    files=request.FILES or None,
+                    instance=post
+                    )
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.save()
+        return redirect('posts:post_detail', post_id=post_id)
+    context = {
+        'form': form,
+        'post': post,
+    }
+    return render(request, 'posts/create_post.html', context)
+
+
+def profile(request, username):
+    user = get_object_or_404(User, username=username)
+    posts = user.posts.all()
+    paginator = Paginator(posts, settings.PAGE_POST)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    num_post = user.posts.all().count()
+    following = user.following.all().exists()
+    context = {
+        'author': user,
+        'page_obj': page_obj,
+        'num_post': num_post,
+        'following': following,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    form = CommentForm(request.POST or None)
+    comments = post.comments.all()
+    count = Post.objects.filter(author=post.author).count()
+    post_object = Post.objects.get(id=post_id)
+    post_object.post_views = post_object.post_views + 1
+    post_object.save()
+    context = {
+        'post': post,
+        'count': count,
+        'form': form,
+        'comments': comments,
+    }
+    return render(request, 'posts/post_detail.html', context)
+
+
+def post_delete(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if post.author == request.user:
+        post.delete()
+    else:
+        return redirect('users:login')
+    return redirect('posts:profile', username=request.user)
+
+
+@login_required
+def add_comment(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    form = CommentForm(request.POST or None)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.author = request.user
+        comment.post = post
+        comment.save()
+    return redirect('posts:post_detail', post_id=post_id)
+
+
+@login_required
+def follow_index(request):
+    post_list = Post.objects.filter(author__following__user=request.user)
+    paginator = Paginator(post_list, settings.PAGE_POST)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/follow.html', context)
+
+
+@login_required
+def profile_follow(request, username):
+    author = get_object_or_404(User, username=username)
+    if author == request.user:
+        return redirect('posts:profile', username=request.user)
+    Follow.objects.get_or_create(user=request.user, author=author)
+    return redirect('posts:profile', username=author)
+
+
+@login_required
+def profile_unfollow(request, username):
+    author = get_object_or_404(User, username=username)
+    obj = Follow.objects.get(
+        user=request.user, author=author)
+    obj.delete()
+    return redirect('posts:profile', username=author)
